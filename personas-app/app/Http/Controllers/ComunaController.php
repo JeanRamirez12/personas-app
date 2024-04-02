0b07e31a232cea3528d36b5b95bf5ffc9d381ed8
JeanRamirez12 <147953086+JeanRamirez12@users.noreply.github.com>
2024-04-02 11:12:43 -0500

    Municipio List

--- personas-app/app/Http/Controllers/ComunaController.php
+++ personas-app/app/Http/Controllers/ComunaController.php
@@ -119,15 +119,16 @@ class ComunaController extends Controller
      */
     public function destroy($id)
     {
-        $comuna = Comuna::find($id);
-        $comuna->delete();
 
-        $comunas = DB::table('tb_comuna')
-        ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
-        ->select('tb_comuna.*', 'tb_municipio.muni_nomb')
-        ->get();
+       $comuna=Comuna::find($id);
+       $comuna->delete();
 
-        return view('comunas.index', ['comunas'  => $comunas]);
+       $comunas = DB::table('tb_comuna')
+       ->join('tb_municipio', 'tb_comuna.muni_codi', '=' ,'tb_municipio.muni_codi')
+       ->select('tb_comuna.*', "tb_municipio.muni_nomb")
+       ->get();
+        
+       return view('comunas.index',['comunas' => $comunas]);
     }
 
 }
