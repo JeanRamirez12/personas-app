fe57afa0675cb93ea18c94e56f0600687897922d
JeanRamirez12 <147953086+JeanRamirez12@users.noreply.github.com>
2024-04-02 10:36:54 -0500

    index Municipio

--- personas-app/resources/views/municipios/index.blade.php
+++ personas-app/resources/views/municipios/index.blade.php
@@ -7,52 +7,51 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
     <title>Hello, world!</title>
   </head>
   <body>
     <div class="container">
       <h1>Listado de comunas</h1>
-      <a href="{{ route('comunas.create') }}" class="btn btn-success">Add</a>
+      <a href="{[ route('municipios.create') ]}" class="btn btn-success">Add</a>
     <table class="table">
    <thead>
      <tr>
      <th scope="col">Code</th>
-     <th scope="col">Commune</th>
      <th scope="col">Municipality</th>
+     <th scope="col">department</th>
+     <th scope="col">country</th>
      <th scope="col">Actions</th>
       </tr>
    </thead>
     <tbody>
-      @foreach ($comunas as $comuna)
+      @foreach ($municipios as $municipio)
         <tr>
-          <th scope="row">{{$comuna->comu_codi}}</th>
-          <td>{{ $comuna->comu_nomb }}</td>
-          <td>{{ $comuna->muni_nomb }}</td>
-          <td>
-            <a href="{{route('comunas.edit',['comuna'=>$comuna->comu_codi]) }}"
-              class="btn btn-info">edit</a></li>
+          <th scope="row">{{$municipio->muni_codi}}</th>
+          <td>{{ $municipio->muni_nomb }}</td>
+          <td>{{ $municipio->muni_nomb }}</td>
+          <td>{{ $municipio->muni_nomb }}</td>
+          <td><span>Actions</span></td>
+      @endforeach
+        </tbody>
+        </table>
+        </div>
 
-            <form action="{{route('comunas.destroy',['comuna'=>$comuna->comu_codi])}}"
-              method="POST" style="display: inline-block">
-              @method('delete')
-              @csrf
-              <input class="btn btn-danger" type="submit" value="delete">
-            </form>
-          </td>
-         </tr>
-       @endforeach
-    </tbody>
- </table>
-</div>
 
+
+
+
+
+
+    
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
   </body>
 </html>
-
-
-
